b5863e11c530506cc59c0332da50c1f283c57aac
Bo <zboyang@umich.edu>
2021-05-14 17:56:41 -0400

    Code push

---
+++ examples/SEEL_gateway_node/SEEL_gateway_node.ino
@@ -0,0 +1,124 @@
+#include <SEEL_Scheduler.h>
+#include <SEEL_GNode.h>
+
+#include <FileIO.h>
+
+/* SEEL Parameters */
+const uint8_t SEEL_TDMA_SLOT_ASSIGNMENT = 0; // TDMA transmission slot, ignored if not using TDMA sending scheme. See SEEL documentation for advised slot configuration.
+
+// Dictates how long SNODEs will be awake/asleep for. Sleep time is calculated by cycle time - awake time, both are converted to millis during calculation
+// Make sure awake/sleep times can be converted to millis without overflow
+const uint32_t SEEL_BCAST_PERIOD_MILLIS = 60000;
+const uint32_t SEEL_SNODE_AWAKE_TIME_SECONDS = 30; // Units are in seconds to minimally fit in bcast packet
+
+/* RF95 Pin Assignments */
+const uint8_t RFM95_CS = 10; // Don't change these if using Dragino LG01
+const uint8_t RFM95_INT = 2; // Don't change these if using Dragino LG01
+
+/* File Write Paramters */
+const char* LOG_FILE_PATH = ""; // Path on gateway node file system
+
+/* SEEL Variables */
+SEEL_Scheduler seel_scheduler;
+SEEL_GNode seel_gnode;
+
+// Example function on how to write strings to file (must have capable hardware setup)
+void write_to_file(const String& to_write)
+{
+  // Append setting adds on, instead of clearing all previous lines in log
+  File f = FileSystem.open(LOG_FILE_PATH, FILE_APPEND);
+
+  if (f) // If the file opened successfully
+  {
+    // Writes the message to file
+    f.println(to_write);
+
+    // Close file after use because file is opened in this function's scope
+    // Also helps with interruptions (power outage)
+    f.close();
+  }
+  else
+  {
+    SEEL_Print::print(F("FOE: ")); // File Open Error
+  }
+  
+  // Also write the message to console for convenience
+  SEEL_Print::println(to_write);
+}
+
+// This callback function is called right after the GNODE sends out its broadcast message
+// Read/Write Parameter: "msg_data" which is the data packet of the broadcast message
+void user_callback_broadcast(uint8_t msg_data[SEEL_MSG_DATA_SIZE])
+{
+  // The contents of this function are an example of what one can do with this CB function
+  
+  String msg_string = "";
+
+  msg_string = F("BT: "); // Broadcast Time
+  msg_string += String(millis(), DEC) + F("\n");
+
+  msg_string += F("BD: "); // Broadcast Data
+  for (uint32_t i = 0; i < SEEL_MSG_DATA_SIZE; ++i)
+  {
+    msg_string += String(msg_data[i], DEC) + " ";
+  }
+
+  write_to_file(msg_string);
+}
+
+// This callback function is called when the GNODE receives a data message
+// Read-Only Parameter: "msg_data" which is the received data packet
+// Read-Only Parameter: "msg_rssi" contains the RSSI of the received message
+void user_callback_data(const uint8_t msg_data[SEEL_MSG_DATA_SIZE], const int8_t msg_rssi)
+{
+  // The contents of this function are an example of what one can do with this CB function
+  
+  String msg_string = "";
+  
+  for (uint32_t i = 0; i < SEEL_MSG_DATA_SIZE; ++i)
+  {
+    uint8_t field_value = msg_data[i];
+    // Record GNODE reception RSSI if GNODE is first parent
+    if (i == 7 && msg_data[6] == SEEL_GNODE_ID) // Corresponds to message locations hard-coded in the SNODE .ino file
+    {
+      field_value = msg_rssi;
+    }
+    msg_string += String(field_value, DEC) + F("\t");
+  }
+
+  write_to_file(msg_string);
+}
+
+void setup()
+{
+  // Not a great source of entropy: https://www.academia.edu/1161820/Ardrand_The_Arduino_as_a_Hardware_Random-Number_Generator
+  randomSeed(random() * analogRead(0)); // Make sure pin 0 is not used
+  
+  // Enables file logging
+  FileSystem.begin();
+
+  /*
+  // Initialize Serial (Serial based comms)
+  Serial.begin(9600);
+  SEEL_Print.init(&Serial);
+  */
+  // Initialize Console (Console bridge based comms, Dragino)
+  Bridge.begin(115200);
+  Console.begin();
+  SEEL_Print::init(&Console);
+
+  // Initialize gateway node and link logging function
+  seel_gnode.init(&seel_scheduler,  // Scheduler reference
+    user_callback_broadcast, user_callback_data, // Callback functions
+    RFM95_CS, RFM95_INT, // Pins
+    SEEL_BCAST_PERIOD_MILLIS, SEEL_SNODE_AWAKE_TIME_SECONDS, // Cycle info
+    SEEL_TDMA_SLOT_ASSIGNMENT); // TDMA slot assignment
+
+  // Run main loop inside SEEL_Scheduler
+  seel_scheduler.run();
+}
+
+void loop()
+{
+  // Empty because main loop happens in SEEL_Scheduler, called in setup()
+}
---
+++ src/SEEL_Node.cpp
@@ -0,0 +1,318 @@
+/*
+The SEEL repository can be found at: https://github.com/SEEL-Group/SEEL
+Copyright (C) SEEL Group 2021 all rights reserved
+See license file in root folder for more licensing details
+See SEEL_documentation.pdf for protocol description details
+
+File purpose:   See SEEL_Node.h
+*/
+
+#include "SEEL_Node.h"
+
+void SEEL_Node::init(uint32_t n_id, uint32_t ts)
+{
+    _node_id = n_id;
+    _tdma_slot = ts;
+    if (_tdma_slot >= SEEL_TDMA_SLOTS && SEEL_TDMA_USE_TDMA)
+    {
+        SEEL_Print::println(F("Error - TDMA Slot Overflow")); // Error - TDMA SLOTS Overflow
+        while(true);
+    }
+
+    _id_verified = true;
+
+    _task_send.set_inst(this);
+}
+
+void SEEL_Node::rfm_param_init(uint8_t cs_pin, uint8_t int_pin, uint8_t TX_power, uint8_t coding_rate)
+{
+    // Create and initialize RFM module
+    _rf95_ptr = new RH_RF95(cs_pin, int_pin);
+    _rf95_ptr->init() ? SEEL_Print::println(F("RF95 Init Success")) : SEEL_Print::println(F("RF95 Init Fail")); // Init - Success : Init - Fail
+
+    // Set LoRa params, defined in 
+    _rf95_ptr->setFrequency(SEEL_RFM95_FREQ);
+    _rf95_ptr->setSpreadingFactor(SEEL_RFM95_SF);
+    _rf95_ptr->setSignalBandwidth(SEEL_RFM95_BW);
+    _rf95_ptr->setTxPower(TX_power);
+    _rf95_ptr->setCodingRate4(coding_rate);
+    SEEL_Print::println(F("Parameters:")); // Parameters
+    SEEL_Print::print(F("\tFq: ")); SEEL_Print::println(SEEL_RFM95_FREQ);
+    SEEL_Print::print(F("\tSF: ")); SEEL_Print::println(SEEL_RFM95_SF);
+    SEEL_Print::print(F("\tBW: ")); SEEL_Print::println(SEEL_RFM95_BW);
+    SEEL_Print::print(F("\tTX: ")); SEEL_Print::println(TX_power);
+    SEEL_Print::print(F("\tCR: ")); SEEL_Print::println(coding_rate);
+    SEEL_Print::print(F("\tPayload Size (Does not include LoRa headers): ")); SEEL_Print::println(SEEL_MSG_TOTAL_SIZE);
+}
+
+void SEEL_Node::create_msg(SEEL_Message* msg, const uint8_t targ_id, 
+    const uint8_t send_id, const uint8_t cmd)
+{
+    msg->targ_id = targ_id;
+    msg->send_id = send_id;
+    msg->cmd = cmd;
+    msg->seq_num = _seq_num;
+}
+
+void SEEL_Node::create_msg(SEEL_Message* msg, const uint8_t targ_id, 
+    const uint8_t send_id, const uint8_t cmd, uint8_t const * data)
+{
+    create_msg(msg, targ_id, send_id, cmd);
+    memcpy(msg->data, data, SEEL_MSG_DATA_SIZE*sizeof(*data));
+}
+
+void SEEL_Node::buf_to_SEEL_msg(SEEL_Message* msg, uint8_t const * buf)
+{
+    memcpy(&msg->targ_id, buf+SEEL_MSG_TARG_INDEX, SEEL_MSG_TARG_SIZE*sizeof(*buf));
+    memcpy(&msg->send_id, buf+SEEL_MSG_SEND_INDEX, SEEL_MSG_SEND_SIZE*sizeof(*buf));
+    memcpy(&msg->cmd, buf+SEEL_MSG_CMD_INDEX, SEEL_MSG_CMD_SIZE*sizeof(*buf));
+    memcpy(&msg->seq_num, buf+SEEL_MSG_SEQ_INDEX, SEEL_MSG_SEQ_SIZE*sizeof(*buf));
+    memcpy(msg->data, buf+SEEL_MSG_MISC_INDEX, SEEL_MSG_DATA_SIZE*sizeof(*buf));
+}
+
+bool SEEL_Node::rfm_send_msg(SEEL_Message* msg, uint8_t seq_num, uint16_t timeout)
+{
+    msg->seq_num = seq_num;
+
+    _rf95_ptr->send((uint8_t *)msg, SEEL_MSG_TOTAL_SIZE);
+    uint32_t send_time_start = millis();
+    if (timeout == 0)
+    {
+        _rf95_ptr->waitPacketSent();
+    }
+    else
+    {
+        // Returns false if timed out
+        if (!_rf95_ptr->waitPacketSent(timeout))
+        {
+            return false; // Message not sent out
+        }
+    }
+
+    uint32_t send_time_end = millis();
+    SEEL_Print::print(F("<<S: "));
+    print_msg(msg);
+    SEEL_Print::print(F(", Time: "));
+    SEEL_Print::print(send_time_start);
+    SEEL_Print::print(F(", ToA: ")); // Send duration (ToA, time in TX state) estimate
+    SEEL_Print::println(send_time_end - send_time_start);
+    SEEL_Print::flush();
+
+    return true; // Message sent out
+}
+
+bool SEEL_Node::dup_msg_check(SEEL_Message* msg)
+{
+    // Check if there are any matches in dup array
+    // The most important fields in the message are send_id and seq_num
+    // using those two fields allows differentiation among unique messages
+    // the active field is used initially in case we receive a message from 
+    // id 0 that has a seq num of 0
+    bool message_duplicate = false;
+    for (uint32_t i = 0; i < SEEL_DUP_MSG_SIZE && !message_duplicate; ++i)
+    {
+        if (_dup_msgs[i].active &&
+            _dup_msgs[i].seq_num == msg->seq_num &&
+            _dup_msgs[i].send_id == msg->send_id &&
+            _dup_msgs[i].cmd == msg->cmd)
+        {
+            message_duplicate = true;
+        }
+    }
+
+    // Check if necessary to store this message into dup array
+    if (!message_duplicate && SEEL_DUP_MSG_SIZE > 0)
+    {
+        // If this message is not a duplicate then replace the 
+        // oldest element in the duplicate array with this message
+        _dup_msgs[_oldest_dup_index].active = true;
+        _dup_msgs[_oldest_dup_index].send_id = msg->send_id;
+        _dup_msgs[_oldest_dup_index].seq_num = msg->seq_num;
+        _dup_msgs[_oldest_dup_index].cmd = msg->cmd;
+
+        ++_oldest_dup_index;
+        if (_oldest_dup_index >= SEEL_DUP_MSG_SIZE)
+        {
+            _oldest_dup_index -= SEEL_DUP_MSG_SIZE;
+        }
+    }
+    return message_duplicate;
+}
+
+bool SEEL_Node::rfm_receive_msg(SEEL_Message* msg, int8_t& rssi)
+{
+    // Message is available
+    if (_rf95_ptr->available())
+    { 
+        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+        uint8_t buf_len = sizeof(buf);
+
+        // Load message into buffer
+        if (_rf95_ptr->recv(buf, &buf_len))
+        {
+            // Converts raw msg buffer to SEEL_Message
+            buf_to_SEEL_msg(msg, buf);
+
+            // Check if the message has already been seen, to prevent a loop
+            SEEL_Print::print(F(">>R: "));
+            if (!dup_msg_check(msg))
+            {
+                rssi = _rf95_ptr->lastRssi();
+
+                // A valid msg was received
+                print_msg(msg);
+                SEEL_Print::print(F("RSSI: ")); SEEL_Print::println(rssi);
+                return true;
+            }
+            else
+            {
+                SEEL_Print::println(F("Duplicate message")); 
+            }
+            SEEL_Print::flush();
+        }
+    }
+
+    // No valid msg was received
+    return false;
+}
+
+void SEEL_Node::enqueue_ack(SEEL_Message* prev_msg)
+{
+    // ACK messages have no target. Instead, the node IDs that have been ack'd are written in
+    // the data section of the message. Which is filled out before sending.
+    if (_ack_queue.find(prev_msg->send_id) == NULL)
+    {
+        _ack_queue.add(prev_msg->send_id);
+    }
+}
+
+void SEEL_Node::print_msg(SEEL_Message* msg)
+{
+    for (uint32_t i = 0; i < SEEL_MSG_TOTAL_SIZE; ++i)
+    {
+        SEEL_Print::print(((uint8_t*)msg)[i]); SEEL_Print::print(F(" "));
+    }
+}
+
+bool SEEL_Node::try_send(SEEL_Message* to_send_ptr, bool seq_inc)
+{
+    uint8_t seq_num = to_send_ptr->seq_num;
+    // Increment sequence numbers for messages sent by this node
+    // Do not modify for bcast msg
+    if (seq_inc)
+    {
+        seq_num = _seq_num;
+        ++_seq_num;
+    }
+
+    // Not waiting for ack or wait has timed out
+    // If timed out, re-send the same message because it has not been popped
+    if (rfm_send_msg(to_send_ptr, seq_num, SEEL_SEND_TIMEOUT_MILLIS))
+    {
+        // Code reaches here if msg was sent out
+        
+        if (!SEEL_TDMA_USE_TDMA)
+        {
+            _last_msg_sent_time = millis();
+            _msg_send_delay = random(SEEL_EB_MIN_MILLIS, SEEL_EB_INIT_MILLIS * pow(SEEL_EB_EXP_SCALE, _unack_msgs));
+        }
+        return true;
+    }
+
+    // Message failed to send
+    return false;
+}
+
+void SEEL_Node::SEEL_Task_Node_Send::run()
+{	
+    bool can_send = true;
+    uint32_t time_millis = millis();
+
+    // Select which collision avoidance strategy to use
+    if (SEEL_TDMA_USE_TDMA)
+    {
+        uint32_t current_slot = (time_millis % SEEL_TDMA_CYCLE_TIME_MILLIS) / SEEL_TDMA_SLOT_WAIT_MILLIS;
+        can_send = (current_slot == _inst->_tdma_slot) && ((time_millis % SEEL_TDMA_SLOT_WAIT_MILLIS) < SEEL_TDMA_BUFFER_MILLIS);
+    }
+    else // Exponential Backoff
+    {
+        can_send = (time_millis - _inst->_last_msg_sent_time) > _inst->_msg_send_delay;
+    }
+
+    // If cannot send or nothing to send, return
+    if (!can_send || (!_inst->_bcast_avail && _inst->_ack_queue.empty() && _inst->_data_queue.empty()))
+    {
+        _inst->_ref_scheduler->add_task(&_inst->_task_send);
+        return;
+    }
+    // If the code reaches here, a message can be sent out
+    SEEL_Message to_send;
+    SEEL_Message* to_send_ptr = &to_send;
+
+    // Prioritize bcast msgs, then ack msgs, then data/id_check msgs
+    // Note messages in _data_queue may be from previous cycles
+    if (_inst->_bcast_avail)
+    {
+        to_send_ptr = &_inst->_bcast_msg;
+
+        to_send_ptr->send_id = _inst->_node_id;
+
+        to_send_ptr->data[SEEL_MSG_DATA_HOP_COUNT_INDEX] = _inst->_hop_count;
+        to_send_ptr->data[SEEL_MSG_DATA_RSSI_INDEX] = _inst->_path_rssi;
+
+        // Update time info right before the send
+        uint32_t time_millis  = millis();
+        to_send_ptr->data[SEEL_MSG_DATA_TIME_SYNC_INDEX] = (uint8_t) (time_millis >> 24);
+        to_send_ptr->data[SEEL_MSG_DATA_TIME_SYNC_INDEX + 1] = (uint8_t) (time_millis >> 16);
+        to_send_ptr->data[SEEL_MSG_DATA_TIME_SYNC_INDEX + 2] = (uint8_t) (time_millis >> 8);
+        to_send_ptr->data[SEEL_MSG_DATA_TIME_SYNC_INDEX + 3] = (uint8_t) (time_millis);
+
+        if (_inst->try_send(to_send_ptr, false))
+        {
+            _inst->_bcast_avail = false;
+            _inst->_bcast_sent = true;
+        }
+    }
+    else if (!_inst->_ack_queue.empty())
+    {
+        memset(to_send_ptr->data, 0, sizeof(to_send_ptr->data[0]) * SEEL_MSG_DATA_SIZE);
+        // Fill message with as many pending ACK's as possible
+        for (uint32_t i = 0; i < SEEL_MSG_DATA_SIZE && !_inst->_ack_queue.empty(); ++i)
+        {
+            to_send_ptr->data[i] = *_inst->_ack_queue.front();
+            _inst->_ack_queue.pop_front();
+        }
+        _inst->create_msg(to_send_ptr, SEEL_GNODE_ID, _inst->_node_id, SEEL_CMD_ACK);
+
+        _inst->try_send(to_send_ptr, true);
+    }
+    else // DATA or ID_CHECK or FORWARDED message
+    {
+        to_send_ptr = _inst->_data_queue.front();
+        uint32_t msg_cmd = to_send_ptr->cmd;
+
+        // A verified node may still have an join requests in the message queue
+        // If this node is already verified, then do not send and pop the join request from send queue
+        if (msg_cmd == SEEL_CMD_ID_CHECK && _inst->_id_verified)
+        {
+            _inst->_data_queue.pop_front();
+            _inst->_ref_scheduler->add_task(&_inst->_task_send);
+            return;
+        }
+
+        // Any ID_CHECK or DATA msgs need to be sent to this node's parent at THIS cycle,
+        // but queue'd messages might have different parents. So correct the parent at SEND time.
+        // Same with self ID, node may take a suggested ID, so sender should also be corrected
+        to_send_ptr->send_id = _inst->_node_id;
+        to_send_ptr->targ_id = _inst->_parent_id;
+
+        if (_inst->try_send(to_send_ptr, true))
+        {
+            ++(_inst->_unack_msgs);
+            ++(_inst->_data_msgs_sent);
+        }
+        // Do not pop msg from queue until msg is ack'd
+    }
+
+    _inst->_ref_scheduler->add_task(&_inst->_task_send);
+}
